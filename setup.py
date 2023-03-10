52707a5397d3e60ed97388a28d06edf99065181b
unknown <vinodkumar9676150@gmail.com>
2023-03-10 14:41:43 +0530

    update all

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import find_packages,setup
 from typing import List
 
-hypen_d_dot='-e .'
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-    requirments=[]
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
     with open(file_path) as file_obj:
-        requirments=file_obj.readlines()
-        requirments=[req.replace('\n'," ") for req in requirments]
-        if hypen_d_dot in requirments:
-            requirments.remove(hypen_d_dot)
-        return requirments
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Vinod kumar',
-    author_email='vinodkumar9676150@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+name='end_to_end_project',
+version='0.0.1',
+author='Vinod',
+author_email='vinodkumar9676150@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
